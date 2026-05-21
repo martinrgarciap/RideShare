4c242932d97ab9feee709374396cc9c58f8bdece
Martin Garcia <martinrgarciap@gmail.com>
2026-05-20 20:55:55 -0400

    Rabbitmq message and queue durability

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"ride-sharing/services/trip-service/internal/infrastructure/events"
 	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
@@ -47,9 +48,11 @@ func main() {
 
 	log.Println("starting RabbitMQ connection")
 
+	publisher := events.NewTripEventPublisher(rabbitmq)
+
 	// Starting the gRPC server
 	grpcServer := grpcserver.NewServer()
-	grpc.NewGRPCHandler(grpcServer, svc)
+	grpc.NewGRPCHandler(grpcServer, svc, publisher)
 
 	log.Printf("starting gRPC server Trip service on port %s", lis.Addr().String())
 
