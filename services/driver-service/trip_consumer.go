8bf1249fc59dd547e859967bc0936b23bf33d06a
Martin Garcia <martinrgarciap@gmail.com>
2026-05-21 23:14:21 -0400

    Send json messages through using exchanges

---
+++ services/driver-service/trip_consumer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"ride-sharing/shared/contracts"
+	"ride-sharing/shared/messaging"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type tripConsumer struct {
+	rabbitmq *messaging.RabbitMQ
+}
+
+func NewTripConsumer(rabbitmq *messaging.RabbitMQ) *tripConsumer {
+	return &tripConsumer{
+		rabbitmq: rabbitmq,
+	}
+}
+
+func (c *tripConsumer) Listen() error {
+	return c.rabbitmq.ConsumeMessages(messaging.FindAvailableDriversQueue, func(ctx context.Context, msg amqp091.Delivery) error {
+		var tripEvent contracts.AmqpMessage
+		if err := json.Unmarshal(msg.Body, &tripEvent); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		var payload messaging.TripEventData
+		if err := json.Unmarshal(tripEvent.Data, &payload); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		log.Printf("driver received message: %+v", payload)
+		return nil
+	})
+}
--- services/trip-service/internal/infrastructure/events/trip_publisher.go
+++ services/trip-service/internal/infrastructure/events/trip_publisher.go
@@ -2,6 +2,9 @@ package events
 
 import (
 	"context"
+	"encoding/json"
+	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/shared/contracts"
 	"ride-sharing/shared/messaging"
 )
 
@@ -15,6 +18,18 @@ func NewTripEventPublisher(rabbitmq *messaging.RabbitMQ) *TripEventPublisher {
 	}
 }
 
-func (p *TripEventPublisher) PublishTripCreated(ctx context.Context) error {
-	return p.rabbitmq.PublishMessage(ctx, "Hello", "Hello world")
+func (p *TripEventPublisher) PublishTripCreated(ctx context.Context, trip *domain.TripModel) error {
+	payload := messaging.TripEventData{
+		Trip: trip.ToProto(),
+	}
+
+	tripEventJSON, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	return p.rabbitmq.PublishMessage(ctx, contracts.TripEventCreated, contracts.AmqpMessage{
+		OwnerID: trip.UserID,
+		Data:    tripEventJSON,
+	})
 }
