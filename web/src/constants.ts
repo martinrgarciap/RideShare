7e9e3a562dd00d64bc8a827602c847a5c7c3fc92
Martin Garcia <martinrgarciap@gmail.com>
2026-05-26 14:48:53 -0400

    Updated next version

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8081';
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? 'ws://localhost:8081/ws';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ?? "http://34.130.109.13:8081";
+export const WEBSOCKET_URL =
+  process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? "ws://34.130.109.13:8081/ws";
